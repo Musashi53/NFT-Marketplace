13853b7903ebac111cfd7809842ddeb935a5ba9d
YuriMendess <contato.yurimendess@gmail.com>
2022-06-12 10:05:11 -0300

    finishing

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -3,3 +3,20 @@ const navToggle = document.querySelector('.nav');
 const navOpen = () => {
   navToggle.classList.toggle('open__nav');
 }
+
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+});
+
+scrollReveal.reveal(`.home__content`);
+scrollReveal.reveal(
+  `.hidden__cards,
+  .card__collections,
+  .bullets__card,
+  .footer`,
+  {interval: 100}
+);
